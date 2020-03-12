caf3b5d063bcfeff25c5d94dcf7fa0d4420977cc
Robin Shackleton <robinshackleton@Robins-MacBook-Air.local>
2020-03-12 13:27:25 -0500

    cors in app.rb

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,6 @@
 class MapsController < ApplicationController
   def index
       response = HTTParty.get 'https://api.chucknorris.io/jokes/random'
-      render json: JSON.parse(response.body)['value']
+      render json: JSON.parse(response.body)
   end
 end
