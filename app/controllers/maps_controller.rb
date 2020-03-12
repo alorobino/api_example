a8ab344a4011465c6403c04394dfee4dbe468d38
Robin Shackleton <robinshackleton@Robins-MacBook-Air.local>
2020-03-12 11:37:18 -0500

    api begin

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,6 @@
+class MapsController < ApplicationController
+  def index
+      response = HTTParty.get 'https://api.chucknorris.io/jokes/random'
+      render json: JSON.parse(response.body)['value']
+  end
+end
